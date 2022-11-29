7b0d86eafeb69112edd6d70263b8ef75e3f7ee5e
Skyler <119079500+skvlar@users.noreply.github.com>
2022-11-29 15:04:25 -0500

    Got buttons in the interface, added themes
    Still need to get light and dark mode button to work, also added a little code to outline the OnClick function.
    Added light and dark themes, though not implemented yet.

--- app/src/main/java/io/github/awwshoot/betternotesapp/Main.java
+++ app/src/main/java/io/github/awwshoot/betternotesapp/Main.java
@@ -1,5 +1,6 @@
 package io.github.awwshoot.betternotesapp;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 
@@ -11,16 +12,26 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
+import android.view.View;
+import android.widget.Button;
+import android.content.SharedPreferences;
 import io.github.awwshoot.betternotesapp.databinding.ActivityMainBinding;
+import android.widget.Switch;
+import android.widget.CompoundButton;
 
 public class Main extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_DARK_THEME = "dark_theme";
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Handle user preferences for theme
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -46,7 +57,25 @@ public class Main extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        // Handle buttons
+
+        // Handle Google Calendar
+
+
+        // Handle Create New Document
+
+
+        // Handle View All Documents
+
+
+        // Handle Dark Mode
+
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,5 +89,29 @@ public class Main extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    // Handle theme switch
+
+    // Handle ALl Button Clicks
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.buttonGoogleCalendar: {
+                // do google calendar integration
+                break;
+            }
+
+            case R.id.buttonCreateDocument: {
+                // do create document
+                break;
+            }
+
+                case R.id.buttonViewDocuments: {
+                    // do view documents
+                    break;
+                }
+
+        }
     }
 }
