1048df2c9bb933d79d4916a167739667d7b4e935
Phrogger <mldattis@gmail.com>
2022-11-29 22:01:37 -0500

    more methods

--- app/src/main/java/folders/Note.java
+++ app/src/main/java/folders/Note.java
@@ -2,7 +2,10 @@ package folders;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.lang.Exception;
+import java.nio.file.Files;
 
 public class Note {
     static void newN() {
@@ -19,28 +22,62 @@ public class Note {
                     System.out.print("that note already exists");
                 }
             }
-            catch (IOException e) {
+            catch (IOException newE) {
                 System.out.print("error");
-                e.printStackTrace();
+                newE.printStackTrace();
             }
         }
     static void openN() {
-        // get file location
-        // open file
+        try {
+            File target = new File("");
+            // get file location
+            if ( == null) {
+                System.out.print("note does not exist");
+            }
+            else {
+                // open file
+            }
+        }
+        catch (IOException openE) {
+            System.out.print("error");
+            openE.printStackTrace();
+        }
     }
     static void renameN() {
-        // get file location
-        // rename file
-        // save changes
+        try {
+            // get file location
+            File target = new File("");
+            // changing file directory
+            File change = new File("");
+        }
+        catch (Exception renameE) {
+            System.out.print("error");
+            renameE.printStackTrace();
+        }
     }
     static void deleteN() {
-        // get file location
-        // delete file
+        try {
+            // get file location
+            File target = new File("");
+            // remove file from directory
+            target.delete();
+        }
+        catch (Exception deleteE) {
+            System.out.print("error");
+            deleteE.printStackTrace();
+        }
     }
     static void moveN() {
-        // get file location
-        // select new file location
-        // save changes
+        try {
+            // get file location
+            File target = new File("");
+            // changing file directory
+            File change = new File("");
+        }
+        catch (Exception moveE) {
+            System.out.print("error");
+            moveE.printStackTrace();
+        }
     }
     static void editN() {
         // get file location
@@ -51,9 +88,9 @@ public class Note {
             editor.close();
             // save changes
         }
-        catch (IOException e) {
+        catch (IOException editE) {
             System.out.print("error");
-            e.printStackTrace();
+            editE.printStackTrace();
         }
     }
     static void convert() {
@@ -61,6 +98,5 @@ public class Note {
         // save as .md file
         // send to where?
     }
-
 }
 
